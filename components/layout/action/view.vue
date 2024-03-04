d263abd1cf06e779ae3449ea6e14c015408a4f6b
NMTuan <nmtuan@qq.com>
2024-03-04 09:47:45 +0800

    chore: 优化dialog 机制

--- components/layout/action/view.vue
+++ components/layout/action/view.vue
@@ -2,37 +2,34 @@
  * @Author: NMTuan
  * @Email: NMTuan@qq.com
  * @Date: 2024-02-29 09:31:00
- * @LastEditTime: 2024-03-04 07:46:49
+ * @LastEditTime: 2024-03-04 09:38:32
  * @LastEditors: NMTuan
  * @Description: 
  * @FilePath: \nuxtAdmin\components\layout\action\view.vue
 -->
 
 <template>
-    <UModal v-model="isOpen" @open="handlerOpen" @close="handlerClose">
+    <UModal :model-value="isOpen" @close="handlerClose">
         <div class="p-4">
             view
             <div>{{ $route.params }}</div>
             <div>{{ $route.query }}</div>
-            <UButton @click="isOpen = false">close</UButton>
+            <UButton @click="handlerClose">close</UButton>
         </div>
     </UModal>
 </template>
 
 <script setup>
 const router = useRouter()
-const isOpen = ref(true)
-const handlerOpen = () => {
-    console.log('open')
-    isOpen.value = true
-}
+const isOpen = ref(false)
+
 const handlerClose = () => {
-    console.log('close')
-    // router.back()
+    isOpen.value = false
+    router.back()
 }
 
 onMounted(() => {
-    console.log('onMounted')
     isOpen.value = true
 })
+
 </script>
