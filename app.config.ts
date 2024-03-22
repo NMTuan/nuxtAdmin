bdb337c33012b3995f5c29574276c84d616c924c
NMTuan <nmtuan@qq.com>
2024-03-22 17:12:57 +0800

    chore: 调整默认会泽为中性色

--- app.config.ts
+++ app.config.ts
@@ -2,7 +2,7 @@
  * @Author: NMTuan
  * @Email: NMTuan@qq.com
  * @Date: 2024-02-08 16:25:11
- * @LastEditTime: 2024-03-14 19:45:22
+ * @LastEditTime: 2024-03-22 16:19:41
  * @LastEditors: NMTuan
  * @Description:
  * @FilePath: \nuxtAdmin\app.config.ts
@@ -10,6 +10,7 @@
 export default defineAppConfig({
     ui: {
         primary: 'sky',
+        gray: 'neutral',
         notifications: {
             position: 'top-0 bottom-auto'
         },
